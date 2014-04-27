03376d0770af167213c4d7542c5c44f7726fbedd
Diego Dell'Era <ddellera@socialmetrix.com>
2014-04-27 14:02:34 -0300

    modified function comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,7 @@
 ## week 3 - programming assignment 2
 
 ## return a list of functions (sort of 'object methods') to access a
-## sort of 'object' containing a matrix & functions to access its
-## inverse matrix
+## sort of 'object' containing a matrix & its inverse matrix
 makeCacheMatrix <- function(x = matrix()) {
 
     ## initialize cached inversed matrix
@@ -28,20 +27,20 @@ makeCacheMatrix <- function(x = matrix()) {
     list(set = set, get = get, set_inverse = set_inverse, get_inverse = get_inverse)
 }
 
-## return a matrix that is the inverse of 'x' & cache result if
-## necessary
+## return a matrix that is the inverse of 'x'. return cached inverse
+## matrix when available, otherwise calculate it & cache it
 cacheSolve <- function(x, ...) {
 
     ## get inverse from input matrix
     inverse_matrix <- x$get_inverse()
 
-    ## if already calculated, return cached result; else, calculate
-    ## inverse & return it
+    ## if already calculated, return cached result;
     if (!is.null(inverse_matrix)) {
         message("getting cached data")
         return(inverse_matrix)
     }
-    
+
+    ##  inverse matrix was not available => calculate it & cache it
     object_matrix <- x$get()
     inverse_matrix <- solve(object_matrix, ...)
     x$set_inverse(inverse_matrix)
